b28f7a3ef38273d3ee244a93a227802b467413e5
Dayany Yamamoto <dayany.yamamoto@gmail.com>
2024-11-27 15:37:40 -0300

    Finalização Footer; Estrutura do Quero Doar

--- src/Pages/QueroDoar/QueroDoar.jsx
+++ src/Pages/QueroDoar/QueroDoar.jsx
@@ -1,10 +1,24 @@
 
-
+import S from './queroDoar.module.scss'
+import Livro from '../../assets/livroIcone.png'
 
 export default function QueroDoar(){
     return(
-        <section>
-            <h2>Quero Doar</h2>
+        <section className={S.principal}>
+            <section className={S.container}>
+                <h2>Por favor, preencha o formulário com suas informações e as informações do Livro</h2>
+                <form action="">
+                    <div>
+                        <img src={Livro} alt="" />
+                        <h3>Informações do Livro
+                        </h3>
+                    </div>
+                    <input type="text" placeholder='Título' />
+                    <input type="text" placeholder='Categoria' />
+                    <input type="text" placeholder='Autor'/>
+                    <input type="text" placeholder='Link da Imagem'/>
+                </form>
+            </section>
         </section>
     )
 }
